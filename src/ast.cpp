5e54a1b50c6055fe5a4a17ef249d2fd540ddc1e2
jian <1184880285@qq.com>
2025-05-20 18:48:32 +0800

    lv1 local test pass

---
+++ src/ast.cpp
@@ -0,0 +1,56 @@
+#include "ast.hpp"
+
+void CompUnitAST::Dump() const { 
+ std::cout << "CompUnitAST { ";
+ func_def->Dump();
+ std::cout << " }";
+}
+
+void CompUnitAST::Dump(std::ostream &os) const {
+ func_def->Dump(os);
+}
+
+void FuncDefAST::Dump() const {
+ std::cout << "FuncDefAST { ";
+ func_type->Dump();
+ std::cout << ", " << ident << ", ";
+ block->Dump();
+ std::cout << " }";
+}
+
+void FuncDefAST::Dump(std::ostream& os) const { 
+ os << "fun " << "@" + ident << "(): ";
+ func_type->Dump(os);
+ os << "{" << std::endl;
+ block->Dump(os);
+ os << "}" << std::endl;
+}
+
+void FuncTypeAST::Dump() const {
+ std::cout << "FuncTypeAST { " << type << " }";
+}
+
+void FuncTypeAST::Dump(std::ostream& os) const {
+ if (type == "int") {
+  os << "i32 "; 
+ }
+}
+
+void BlockAST::Dump() const { 
+ std::cout << "BlockAST { ";
+ stmt->Dump();
+ std::cout << " }";
+}
+
+void BlockAST::Dump(std::ostream& os) const {
+ os << "\%entry:" << std::endl;
+ stmt->Dump(os);
+}
+
+void StmtAST::Dump() const {
+ std::cout << "StmtAST { " << number << " }";
+}
+
+void StmtAST::Dump(std::ostream& os) const {
+ os << "  ret " << number << std::endl;
+}
---
+++ src/ast.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <iostream>
+#include <typeinfo>
+#include <cstdlib>
+
+// 所有 AST 的基类
+class BaseAST {
+ public:
+  virtual ~BaseAST() = default;
+  virtual void Dump() const = 0;
+  virtual void Dump(std::ostream& os) const = 0;
+};
+
+// CompUnit 是 BaseAST
+class CompUnitAST : public BaseAST {
+ public:
+  // 用智能指针管理对象
+  std::unique_ptr<BaseAST> func_def;
+
+  void Dump() const override;
+  void Dump(std::ostream& os) const override;
+};
+
+// FuncDef 也是 BaseAST
+class FuncDefAST : public BaseAST {
+ public:
+  std::unique_ptr<BaseAST> func_type;
+  std::string ident;
+  std::unique_ptr<BaseAST> block;
+
+  void Dump() const override;
+  void Dump(std::ostream& os) const override;
+};
+
+class FuncTypeAST : public BaseAST {
+ public:
+  std::string type;
+
+  void Dump() const override;
+  void Dump(std::ostream& os) const override;
+};
+
+class BlockAST : public BaseAST {
+ public:
+  std::unique_ptr<BaseAST> stmt;
+  
+  void Dump() const override;
+  void Dump(std::ostream& os) const override;
+};
+
+class StmtAST : public BaseAST {
+ public:
+  int number;
+
+  void Dump() const override;
+  void Dump(std::ostream& os) const override;
+};
